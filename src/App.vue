fc9b6af88ff55095578114b791f08d2ecf8f3296
Rebeca <becatriz7@gmail.com>
2020-09-22 00:29:11 -0400

    Adicionado Vuetify, Rotas e opção de Tema Dark e Light

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,79 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense >
+        <v-list-item link :to="item.route" v-for="(item,index) in routes" :key="index">
+          <v-list-item-action>
+            <v-icon>{{item.icon}}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{item.name}}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Controle de Contas</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-switch v-model="active" :label="`Dark`"></v-switch>
+    </v-app-bar>
+    <v-main>
+       <router-view></router-view>
+    </v-main>
+    <v-footer app>
+      <span>Rebeca Lopes &copy; {{ new Date().getFullYear() }}</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+<script>
+export default {
+  props: {
+    source: String,
+  },
+  data: () => ({
+    routes: [
+      {
+        name:"Home",
+        icon:"mdi-home",
+        route:"/"
+      },
+      {
+        name:"Contas",
+        icon:"mdi-code-string",
+        route:"/accounts"
+      },
+        {
+        name:"Relatórios",
+        icon:"mdi-chart-bar",
+        route:"/reports"
+      },
+        {
+        name:"Usuários",
+        icon:"mdi-account",
+        route:"/users"
+      }
+    ],
+    active: false,
+    drawer: null,
+  }),
+  watch: {
+    active: {
+      handler() {
+        if (this.active) {
+          this.$vuetify.theme.dark = true;
+        } else {
+          this.$vuetify.theme.dark = false;
+        }
+      },
+    },
+  },
+};
+</script>
+<style scoped>
+.v-list{
+  margin-right: 10px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -11,12 +11,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/accounts',
+    name: 'app.accounts',
+    component: () => import('../views/Accounts.vue')
+  },
+  {
+    path: '/reports',
+    name: 'app.reports',
+    component: () => import('../views/Reports.vue')
+  },
+  {
+    path: '/users',
+    name: 'app.users',
+    component: () => import('../views/Users.vue')
   }
 ]
 
